34fbae77292c144552ef29a4160c27498de137c7
Ella Anttila <ella.anttila@aalto.fi>
2019-07-16 16:00:02 +0300

    Add include-exclude-filters and support for three-state-buttons
    Created include-exclude-filters, also has option of choosing whether
    to combine includes with AND or OR.
    CSS now supports three-state-buttons.

--- django_colortag/fields.py
+++ django_colortag/fields.py
@@ -1,6 +1,10 @@
-from django.forms import models
+from django.forms import models, fields
 
-from .widgets import ColortagSelectMultiple
+from .widgets import (
+    ColortagSelectMultiple,
+    ColortagIEMultiWidget,
+    ColortagIEAndOrWidget
+)
 
 
 class ModelChoiceInstanceIterator(models.ModelChoiceIterator):
@@ -18,3 +22,46 @@ class ColortagChoiceField(models.ModelMultipleChoiceField):
 
     def label_from_instance(self, obj):
         return obj.name
+
+
+class ColortagIEField(models.ModelMultipleChoiceField):
+    widget = ColortagIEMultiWidget
+    iterator = ModelChoiceInstanceIterator
+
+    def set_queryset(self, queryset):
+        self.queryset = queryset
+        self.widget.set_subwidgets(queryset)
+
+    def clean(self, value):
+        includes = []
+        excludes = []
+        for v in value:
+            if v == None or len(v) < 2:
+                continue
+            elif v[0] == 'I':
+                includes.append(v[1:])
+            elif v[0] == 'E':
+                excludes.append(v[1:])
+        includes_qs = super().clean(includes)
+        excludes_qs = super().clean(excludes)
+        return (includes_qs, excludes_qs)
+
+
+class ColortagIEAndOrField(fields.MultiValueField):
+    widget = ColortagIEAndOrWidget
+
+    def __init__(self, queryset, *args, **kwargs):
+        kwargs.setdefault('require_all_fields', False)
+        subfields = (
+            fields.BooleanField(required=False),
+            ColortagIEField(queryset, required=False)
+        )
+        super().__init__(subfields, *args, **kwargs)
+
+    def set_queryset(self, queryset):
+        self.queryset = queryset
+        self.fields[1].queryset = queryset
+        self.widget.set_subwidgets(queryset)
+
+    def compress(self, data_list):
+        return data_list
--- django_colortag/filters.py
+++ django_colortag/filters.py
@@ -1,7 +1,47 @@
+from django.db.models import Q
+
 import django_filters
 
-from .fields import ColortagChoiceField
+from .fields import (
+    ColortagChoiceField,
+    ColortagIEField,
+    ColortagIEAndOrField
+)
 
 
 class ColortagChoiceFilter(django_filters.ModelMultipleChoiceFilter):
     field_class = ColortagChoiceField
+
+
+class ColortagIncludeExcludeFilter(django_filters.ModelMultipleChoiceFilter):
+    field_class = ColortagIEField
+
+    def filter(self, qs, values, conjoined=False):
+        includes, excludes = values
+        if not includes and not excludes:
+            return qs
+
+        if not conjoined:
+            q = Q()
+        for v in set(includes):
+            predicate = self.get_filter_predicate(v)
+            if conjoined:
+                qs = qs.filter(**predicate)
+            else:
+                q |= Q(**predicate)
+        if not conjoined:
+            qs = qs.filter(q)
+
+        for v in set(excludes):
+            predicate = self.get_filter_predicate(v)
+            qs = qs.exclude(**predicate)
+
+        return qs.distinct() if self.distinct else qs
+
+
+class ColortagIEAndOrFilter(ColortagIncludeExcludeFilter):
+    field_class = ColortagIEAndOrField
+
+    def filter(self, qs, values):
+        conjoined, inc_exc = values
+        return super().filter(qs, inc_exc, conjoined=conjoined)
